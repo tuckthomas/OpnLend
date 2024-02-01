40ac850fb36d82e4e8bb0c4db85f2257990195aa
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-01 01:10:36 -0500

    Updated Income Statement and Balance Sheet HTML

--- Financial_Manager/views.py
+++ Financial_Manager/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
-# Renders Income Statement
+# Renders Business Financial Statement with (Context) Session Data
 def Financials(request):
-    return render(request, 'Financials.html')
+    context = {
+        'Global_Relationship_ID': request.session.get('Global_Relationship_ID', ''),
+        'Meta_ID': request.session.get('Meta_ID', ''),
+        'Unique_ID': request.session.get('Unique_ID', ''),
+        'Unique_Jointly_Reported_ID': request.session.get('Unique_Jointly_Reported_ID', '')
+    }
+    return render(request, 'Financials.html', context)
