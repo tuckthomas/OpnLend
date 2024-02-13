b3f2a749672b278119980a4fc99939f7d6a43e7e
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-12 21:38:48 -0500

    Update apps.py

--- UserProfiles/apps.py
+++ UserProfiles/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
-class UserprofilesConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'UserProfiles'
+    name = 'Authentication'
+
+    def ready(self):
+        # This imports the signals to ensure they are connected
+        import Authentication.signals
