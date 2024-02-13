69b1c059bc01419c93b0b9c77bb44da1931e5403
Tucker Thomas Olson <90152609+tuckthomas@users.noreply.github.com>
2024-02-12 22:07:47 -0500

    Update api.py

--- OpnLend/api.py
+++ OpnLend/api.py
@@ -1,14 +1,15 @@
 # OpnLend API
-# Centralized endpoints
+# Centralized Endpoints
 
 from ninja import NinjaAPI
 from Global_Relationships.api import router as global_relationships_router
-
+# Import the router from UserProfiles app
+from UserProfiles.api import router as userprofiles_router
 
 api = NinjaAPI(title="OpnLend API", version="1.0.0")
 
 # Including routers from each app
 api.add_router("/global-relationships/", global_relationships_router)
+api.add_router("/user-profiles/", userprofiles_router)
 
-# Note: Ensure that each `api.py` within the apps defines a Router instance.
-# For apps without API endpoints, you simply won't include them here.
+# Note: Each `api.py` within the apps defines a Router instance and must be included above
